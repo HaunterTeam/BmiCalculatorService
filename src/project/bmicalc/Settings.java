927efd4ea88009ea6acb63ca23a3a155a2daca27
Roberto Zen <rob.zenn92@hotmail.it>
2015-02-10 21:28:04 +0100

    Settings updated

--- src/project/bmicalc/Settings.java
+++ src/project/bmicalc/Settings.java
@@ -7,7 +7,7 @@ public final class Settings {
 //	public static final String BASE_PORT = "5900";
 	
 	public static final String BASE_PROTOCOL = "http://";
-	public static final String BASE_URL = "localhost";
+	public static final String BASE_URL = "188.226.183.46";
 	public static final String BASE_PORT = "8030";
 	public static final String SERVICE_PATH = "/bmicalc-service/";
 	
